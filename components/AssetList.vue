795835987902cfd2c03247a413c402ef95b975ae
EJ Fox <ejfox@ejfox.com>
2022-11-28 12:15:32 -0500

    style: add satellite photos to asset list

--- components/AssetList.vue
+++ components/AssetList.vue
@@ -1,11 +1,18 @@
 <template>
 <div class="sans-serif f3">
   <div v-if="assets">
-    <div v-for="asset in assets" :key="asset.id" class="w-50 dib v-top"
+    <div v-for="asset in assets" :key="asset.id" class="w-50 dib v-top pa0 ba b--white"
+    :style="{
+      backgroundImage: `url(${asset.properties['ImageUrl']})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }"
     @click="addToShoppingCart(asset)">
-    <h3 class="pointer">{{asset.properties.Name}}</h3>
-    <h4 class="gray">100yr co2: {{asset.properties['EmissionValue']}}</h4>
+    <div class="asset-inner-container h5 pa4">
+    <h3 class="pointer mv0">{{asset.properties.Name}}</h3>
+    <h4 class="dark-gray">100yr co2: {{asset.properties['EmissionValue']}}</h4>
       <!-- <pre class="h5 overflow-y-auto ba b--red">{{asset}}</pre> -->
+    </div>
     </div>
   </div>
   <div v-else>
@@ -36,3 +43,8 @@ onMounted(() => {
     });
 })
 </script>
+<style>
+.asset-inner-container {
+  background-color: rgba(255,255,255,0.45);  
+}
+</style>
